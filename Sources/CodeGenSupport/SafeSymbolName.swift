e019447c99f59efcfd9c493ea2c6800a05527b03
akoslowski <2005824+akoslowski@users.noreply.github.com>
2024-11-25 22:39:08 +0100

    Update Swift keywords

--- Sources/CodeGenSupport/SafeSymbolName.swift
+++ Sources/CodeGenSupport/SafeSymbolName.swift
@@ -2,35 +2,85 @@ import Foundation
 
 public func safeSymbolName(_ name: String) -> String {
     let newName = name.replacingOccurrences(of: ".", with: "_")
-    let reservedWords = [
-        "actor",
-        "associatedtype",
-        "class",
-        "deinit",
-        "enum",
-        "extension",
-        "fileprivate",
-        "func",
-        "import",
-        "init",
-        "inout",
-        "internal",
-        "let",
-        "open",
-        "operator",
-        "private",
-        "precedencegroup",
-        "protocol",
-        "public",
-        "rethrows",
-        "static",
-        "struct",
-        "subscript",
-        "typealias",
-        "var"
-    ]
-    if reservedWords.contains(newName) {
+    if keywords.contains(newName) {
         return newName.tickQuoted()
     }
     return newName
 }
+
+// https://docs.swift.org/swift-book/documentation/the-swift-programming-language/lexicalstructure/#Keywords-and-Punctuation
+
+let keywords = keywordsUsedInDeclarations
+    .union(keywordsUsedInStatements)
+    .union(keywordsUsedInExpressionsAndTypes)
+
+let keywordsUsedInDeclarations: Set<String> = [
+    "actor",
+    "associatedtype",
+    "borrowing",
+    "class",
+    "consuming",
+    "deinit",
+    "enum",
+    "extension",
+    "fileprivate",
+    "func",
+    "import",
+    "init",
+    "inout",
+    "internal",
+    "let",
+    "nonisolated",
+    "open",
+    "operator",
+    "private",
+    "precedencegroup",
+    "protocol",
+    "public",
+    "rethrows",
+    "static",
+    "struct",
+    "subscript",
+    "typealias",
+    "var",
+]
+
+let keywordsUsedInStatements: Set<String> = [
+    "break",
+    "case",
+    "catch",
+    "continue",
+    "default",
+    "defer",
+    "do",
+    "else",
+    "fallthrough",
+    "for",
+    "guard",
+    "if",
+    "in",
+    "repeat",
+    "return",
+    "throw",
+    "switch",
+    "where",
+    "while",
+]
+
+let keywordsUsedInExpressionsAndTypes: Set<String> = [
+    "Any",
+    "as",
+    "await",
+    "catch",
+    "false",
+    "is",
+    "nil",
+    "rethrows",
+    "self",
+    "Self",
+    "super",
+    "throw",
+    "throws",
+    "true",
+    "try"
+]
