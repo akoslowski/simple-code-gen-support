333cee9c367abba3cead42e3f438679a363ee6ac
Andreas Koslowski <2005824+akoslowski@users.noreply.github.com>
2024-11-28 10:00:01 +0100

    Add code preview (#1)
    * Add code preview
    
    * Update Swift keywords

--- Package.swift
+++ Package.swift
@@ -1,16 +1,30 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
     name: "CodeGenSupport",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v14)],
     products: [
         .library(
             name: "CodeGenSupport",
-            targets: ["CodeGenSupport"]),
+            targets: ["CodeGenSupport"]
+        ),
+        .library(
+            name: "CodePreview",
+            targets: ["CodePreview"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/JohnSundell/Splash", from: "0.16.0")
     ],
     targets: [
+        .target(
+            name: "CodePreview",
+            dependencies: [
+                .product(name: "Splash", package: "Splash")
+            ]
+        ),
         .target(
             name: "CodeGenSupport"),
         .testTarget(
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Code Generation Support
 
 `CodeGenSupport` package contains a collection of simple helpers that can be used in the context of generating Swift code programmatically.
+
+`CodePreview` is a simple helper that can be used to preview generated code in a SwiftUI preview.
